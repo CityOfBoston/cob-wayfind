af725e6ebd54cd283ea92ed39fb554092ed5d2db
Matthew Crist <matthew.crist@boston.gov>
2016-08-22 14:34:10 -0400

    Move CityScore to the footer

--- _gulp/tasks/browserSync.js
+++ _gulp/tasks/browserSync.js
@@ -6,6 +6,14 @@ gulp.task('browser-sync', function() {
   browserSync.init(['css/*.css', 'js/**/*.js', 'index.html'], {
     server: {
       baseDir: './_site'
+    },
+    snippetOptions: {
+      rule: {
+        match: /<\/head>/i,
+        fn: function (snippet, match) {
+          return snippet + match;
+        }
+      }
     }
   });
 });
