4599b52b2aea62f7dda0c83d181a51720bd4afe8
Matthew Crist <matthew.crist@boston.gov>
2016-08-23 12:27:15 -0400

    Update URLs

--- _plugins/env.rb
+++ _plugins/env.rb
@@ -5,8 +5,9 @@ module Jekyll
   class EnvironmentVariablesGenerator < Generator
 
     def generate(site)
-      site.config['pusher_app_key'] = ENV['PUSHER_APP_KEY'] || 'nothing to see here'
-      site.config['pusher_channel'] = ENV['PUSHER_CHANNEL'] || 'wha wha what'
+      site.config['pusher_app_key'] = ENV['PUSHER_APP_KEY'] || '080d9fdb5958cef6a952'
+      site.config['pusher_channel'] = ENV['PUSHER_CHANNEL'] || 'dev_channel'
+      site.config['events_api'] = ENV['EVENTS_URL'] || 'https://hub.dd:8443/api/v1/upcoming-events'
     end
 
   end
